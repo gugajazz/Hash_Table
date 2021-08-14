ee8dbdacc978aba2d3c861b9050a5a0b8449c231
Gustavo Poção <gugapocao@gmail.com>
2021-08-14 14:42:02 +0100

    added linear search and timing

--- hashtable_linear.c
+++ hashtable_linear.c
@@ -9,27 +9,49 @@ struct node{
 };
 typedef struct node node;
 
-int size = 3000;
+int size = 3000000;
 
 int hash(int key){
     return (key % size);
+}
+
+void find_linear_search(int key, node *hashtable){
+    clock_t begin = clock();
+
+    for(int i=0; i<size; i++){
+        if(hashtable[i].key==key){
+            printf("Key/Value pair in index %d\n",i); 
+            clock_t end = clock();
+            double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; 
+            printf("Time-> %f\n",time_spent);
+            return;
+        }
+    }
+    
+    printf("Key %d not in hashtable\n", key);
+    return;
+    
 }
 
 void find(int key, node *hashtable){
     clock_t begin = clock();
 
     int index = hash(key);
-    if(hashtable[index].key == key){
+    if(hashtable[index].key == key){ // se tiver sido guardado sem colision
         printf("Key/Value pair in index %d\n",index); 
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; 
+        printf("Time-> %f\n",time_spent);
     }
     else{
-        for(int i=index, k=0; i<size; i++){ 
+        for(int i=index, k=0; i<size; i++){ // se tiver sido guardado com colision
             if(hashtable[i].key == key){
                 printf("Key/Value pair in index %d\n",i); 
             
                 clock_t end = clock();
-                double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+                double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; 
                 printf("Time-> %f\n",time_spent);
+
                 return;
             }
             else if(i==size-1){
@@ -93,6 +115,7 @@ void show(node *hashtable){
 }
 
 void main(){
+    printf("UIII");
     node hashtable[size];
     char input[10]; 
     int key, loop=1, checking_input=1;
@@ -100,7 +123,7 @@ void main(){
     initialize_hashtable(hashtable);
 
     while(loop){
-        printf("\nAdd key/value(1)\nRemove key/value(2)\nSee hashtable(3)\nSearch by key(4)\nexit(5)\n-> ");
+        printf("\nAdd key/value(1)\nRemove key/value(2)\nSee hashtable(3)\nSearch by key(4)\nexit(5)\nLinear search(6)\n-> ");
         fgets(input, 2, stdin);
         while ((getchar()) != '\n'); //clear stdin buffer
         switch (input[0]){
@@ -154,6 +177,23 @@ void main(){
 
             case '5':
                 exit(1);
+                break;
+
+            case '6':
+                checking_input=1;
+                while(checking_input){
+                    printf("\nInsert key (MAX 9 CHARS): ");
+                    fgets(input,10,stdin);                    
+                    key = atoi(input); //convert str to int         
+                    if(key==0 && input[0] != '0'){
+                        printf("Invalid key\n");
+                        checking_input = 1;
+                    }
+                    else{
+                        checking_input=0;
+                    }
+                }
+                find_linear_search(key,hashtable);
                 break;
         }   
     }
